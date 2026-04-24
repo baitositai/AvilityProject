4e1ff2cb48f90a67440df5e37694cf727900b9df
ASOST\2316013 <nsagara1110@gmail.com>
2026-04-24 14:33:53 +0900

    Chargeショット基礎実装

---
+++ Script/Component/Avility/ComponentAvilityChargeShot.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "ComponentAvilityBase.h"
+#include "../../Common/Vector2F.h"
+
+class InputManager;
+class Player;
+
+class ComponentAvilityChargeShot :
+    public ComponentAvilityBase
+{
+public:
+
+	static constexpr float SHOT_INTERVAL = 0.5f;	// ショットのインターバル
+	static constexpr float SHOT_SPEED = 15.0f;		// ショットの速度
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="owner">所有者</param>
+	ComponentAvilityChargeShot(Player& owner);
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~ComponentAvilityChargeShot() override;
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update() override;
+
+private:
+
+	// 入力管理クラスの参照
+	InputManager& inputManager_;
+
+	// 移動量
+	Vector2F moveAmount_;
+
+	// 移動入力処理
+	void ProcessInputShot();
+	void ProcessMoveShot();
+
+	// 状態関数マップ
+	std::unordered_map<std::string, std::function<void(void)>> stateFunctionMap_;
+	// 現在の状態
+	std::string currentState_;
+	std::function<void(void)> currentStateFunction_;
+
+	// ショット変数
+	float shotTime_;
+	Vector2F shotVec_;
+
+};
+
